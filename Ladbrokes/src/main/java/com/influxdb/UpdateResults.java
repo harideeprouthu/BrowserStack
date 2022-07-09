9ddeede11d6cfe79f8e5c0c0a7058577e6fdf367
ADMIN <ADMIN@DESKTOP-P5217BT.mshome.net>
2022-07-09 22:35:34 +0530

    Core Project

---
+++ Ladbrokes/src/main/java/com/influxdb/UpdateResults.java
@@ -0,0 +1,21 @@
+package com.influxdb;
+
+
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+
+public class UpdateResults {
+
+  private static final InfluxDB INFLXUDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+  private static final String DB_NAME = "Browserstack";
+
+  static {
+    INFLXUDB.setDatabase(DB_NAME);
+  }
+
+  public static void post(final Point point) {
+    INFLXUDB.write(point);
+  }
+
+}
