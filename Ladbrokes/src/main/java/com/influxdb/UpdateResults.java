cf27db9052a4136881ecf2cbb43097083ac6fc70
harideeprouthu <108993679+harideeprouthu@users.noreply.github.com>
2022-07-20 21:51:20 +0530

    updated db url

--- Ladbrokes/src/main/java/com/influxdb/UpdateResults.java
+++ Ladbrokes/src/main/java/com/influxdb/UpdateResults.java
@@ -7,7 +7,7 @@ import org.influxdb.dto.Point;
 
 public class UpdateResults {
 
-  private static final InfluxDB INFLXUDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+  private static final InfluxDB INFLXUDB = InfluxDBFactory.connect("http://10.102.97.210:8086", "root", "root");
   private static final String DB_NAME = "Browserstack";
 
   static {
